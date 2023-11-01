7b0c81fea9f80958eb841bec493f45f6708fb794
mathi00 <148368513+mathi00@users.noreply.github.com>
2023-11-01 11:13:05 +0530

    Add files via upload

---
+++ diabetes_prediction.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+file_path = "diabetes.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataset = pd.read_csv(file_path, names=names)
+
+X = dataset.drop('class', axis=1)
+y = dataset['class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy:.2f}")
+
+report = classification_report(y_test, y_pred)
+print("Classification Report:\n", report)
